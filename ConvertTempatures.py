511b745bc3ab8269a8a1c1691b9a517283913f79
Rockstreamgirl <33559506+rockstreamgirl@users.noreply.github.com>
2017-11-10 18:13:48 -0500

    Capitalize Celsius

--- ConvertTempatures.py
+++ ConvertTempatures.py
@@ -1,3 +1,3 @@
 fahrenheit = float (raw_input("What is the tempature Fahrenheit? "))
 celsius = 5/9.0 * (fahrenheit - 32)
-print "That equals", celsius, "degrees celsius."
+print "That equals", celsius, "degrees Celsius."

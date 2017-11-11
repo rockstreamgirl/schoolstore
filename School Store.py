6869d1e435a650ff45b6d0c91a87c0579f46d7fa
RockStreamGuy <33560692+rockstreamguy@users.noreply.github.com>
2017-11-10 19:15:15 -0500

    Added School Store.py

---
+++ School Store.py
@@ -0,0 +1,29 @@
+
+print "*STORE ITEMS*"
+print ""
+print "doritos"
+print "coffee"
+print "coke"
+print "pepsi"
+print "pringles"
+print "gum"
+print "brownie"
+
+doritos=1.00
+coffee=2.00
+coke=1.00
+pepsi=1.00
+pringles=0.50
+gum=0.25
+brownie=0.75
+
+store=[doritos, coffee, coke, pepsi, pringles, gum, brownie]
+
+print ""
+order= input ("What is the order? ")
+print order
+
+print ""
+
+c=input ("Change? *PLACE MONEY GIVEN* ")
+print c-order
